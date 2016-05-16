1264dd9ce66ac970855803857ab460b2269b5f15
salavi <sh.alavi77@gmail.com>
2016-05-16 12:24:42 +0430

    Authentication Package is added.

---
+++ ResourceManagementSystem/src/authentication/Authentication.java
@@ -0,0 +1,17 @@
+package authentication;
+
+import java.awt.Label;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Authentication {
+
+	public static void main(String[] args) {
+		JFrame startUpPageJF = new JFrame();
+		JPanel startUpPageJP = new JPanel();
+		Label label = new Label("Welcome !");
+	}
+	
+}
+
