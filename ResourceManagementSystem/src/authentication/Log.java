bf016f9c07be88575b31bd5425238b7c50bfa08a
salavi <sh.alavi77@gmail.com>
2016-06-16 15:30:49 +0430

    Architectural BaseLine

--- ResourceManagementSystem/src/authentication/Log.java
+++ ResourceManagementSystem/src/authentication/Log.java
@@ -1,59 +1,58 @@
 package authentication;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import org.apache.log4j.BasicConfigurator;
 
 public class Log extends JFrame {
 
-public static void main(String[] args) {
-Log frameTabel = new Log();
-}
+	public static void main(String[] args) {
+		Log frameTabel = new Log();
+	}
 
-JButton blogin = new JButton("Login");
-JPanel panel = new JPanel();
-JTextField txuser = new JTextField(15);
-JPasswordField pass = new JPasswordField(15);
+	JButton blogin = new JButton("Login");
+	JPanel panel = new JPanel();
+	JTextField txuser = new JTextField(15);
+	JTextField pass = new JTextField(15);
 
-Log(){
-super("Login Autentification");
-setSize(300,200);
-setLocation(500,280);
-panel.setLayout (null); 
+	Log() {
+		super("Login Autentification");
+		setSize(300, 200);
+		setLocation(500, 280);
+		panel.setLayout(null);
 
+		txuser.setBounds(70, 30, 150, 20);
+		pass.setBounds(70, 65, 150, 20);
+		blogin.setBounds(110, 100, 80, 20);
 
-txuser.setBounds(70,30,150,20);
-pass.setBounds(70,65,150,20);
-blogin.setBounds(110,100,80,20);
+		panel.add(blogin);
+		panel.add(txuser);
+		panel.add(pass);
 
-panel.add(blogin);
-panel.add(txuser);
-panel.add(pass);
+		getContentPane().add(panel);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		actionlogin();
+	}
 
-getContentPane().add(panel);
-setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-setVisible(true);
-actionlogin();
-}
-
-public void actionlogin(){
-blogin.addActionListener(new ActionListener() {
-public void actionPerformed(ActionEvent ae) {
-String puname = txuser.getText();
-String ppaswd = pass.getText();
-if(puname.equals("test") && ppaswd.equals("12345")) {
-newframe regFace =new newframe();
-regFace.setVisible(true);
-dispose();
-} else {
-
-JOptionPane.showMessageDialog(null,"Wrong Password / Username");
-txuser.setText("");
-pass.setText("");
-txuser.requestFocus();
-}
-
-}
-});
-}
+	public void actionlogin() {
+		blogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				String puname = txuser.getText();
+				String ppaswd = pass.getText();
+				System.out.println(puname + " " + ppaswd);
+				StoreData.initial();
+				StoreData.register(puname, ppaswd);
+				StoreData.close();
+			}
+		});
+	}
 }
