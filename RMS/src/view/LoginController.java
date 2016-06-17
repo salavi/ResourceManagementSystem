37c5b0d1b6b3be284c0f9564dfabc1cb7a7ab8c6
Hooori <hourie.marefat@gmail.com>
2016-06-17 17:42:46 +0430

    Before LoginController.java is added

---
+++ RMS/src/view/LoginController.java
@@ -0,0 +1,41 @@
+package view;
+
+import controller.MainApp;
+import javafx.fxml.FXML;
+
+class LoginController {
+	// Reference to the main application.
+    private MainApp mainApp;
+
+    
+    /**
+     * The constructor.
+     * The constructor is called before the initialize() method.
+     */
+    public LoginController() {
+    }
+    
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+        // Initialize the person table with the two columns.
+        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+    }
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     * 
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+
+        // Add observable list data to the table
+        personTable.setItems(mainApp.getPersonData());
+    }
+    
+}
