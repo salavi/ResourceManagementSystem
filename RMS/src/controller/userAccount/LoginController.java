4d286668ac3305425e7b13a2736a51515e9eb45e
salavi <sh.alavi77@gmail.com>
2016-07-07 20:52:42 +0430

    showHomePage() is added to LoginController

--- RMS/src/controller/userAccount/LoginController.java
+++ RMS/src/controller/userAccount/LoginController.java
@@ -15,80 +15,83 @@ import javafx.stage.Stage;
 import logic.authentication.Authentication;
 import logic.authentication.UserAccount;
 
-public class LoginController {	  
+public class LoginController {
 	private MainApp mainApp; // Reference to the main application.
-	@FXML private TextField username;
-	@FXML private TextField password;
-	@FXML private Button loginButton;
-	@FXML private Button signUpButton;
-	@FXML private Text message;
+	@FXML
+	private TextField username;
+	@FXML
+	private TextField password;
+	@FXML
+	private Button loginButton;
+	@FXML
+	private Button signUpButton;
+	@FXML
+	private Text message;
 
 	private Authentication auth;
 	private UserAccount loggedInUser;
 
-
 	/**
-	 * The constructor.
-	 * The constructor is called before the initialize() method.
+	 * The constructor. The constructor is called before the initialize()
+	 * method.
 	 */
 	public LoginController() {
 		auth = new Authentication();
-		message  = new Text();
+		message = new Text();
 	}
-
-
 
 	/**
 	 * Called when there is an action on LoginButton
 	 */
 	@FXML
 	private void handleLoginButton() {
-		if(!username.getText().isEmpty() && !password.getText().isEmpty()){
+		if (!username.getText().isEmpty() && !password.getText().isEmpty()) {
 			loggedInUser = auth.login(username.getText(), password.getText());
-			if(loggedInUser == null){
+			if (loggedInUser == null) {
 				message.setFill(Color.RED);
 				message.setText("کاربر با این مشخصات موجود نمی‌باشد");
-			}
-			else{
-				//TODO
+			} else {
+				// TODO
 				message.setFill(Color.GREEN);
 				message.setText("ورود شما با موفقیت انجام شد");
-				try{
-					Stage stage; 
-					Parent root;
-					stage = (Stage) loginButton.getScene().getWindow();
-					FXMLLoader loader = new FXMLLoader();
-					loader.setLocation(getClass().getResource("../../view/userAccount/UserAccount.fxml"));
-					root = (Parent) loader.load();
-					UserAccountController userAccountController = loader.<UserAccountController>getController();
-					userAccountController.setLoggedInUser(loggedInUser);
-					userAccountController.getProfileController().setLoggedInUser(loggedInUser);
-					userAccountController.getProfileController().setAllLabels();
-			
-					Scene scene = new Scene(root);
-					stage.setScene(scene);
-					stage.show();	
-				}
-				catch(Exception e){
-					e.printStackTrace();
-				}
+				showHomePage();
 			}
-		}
-		else{
+		} else {
 			message.setFill(Color.RED);
 			message.setText("لطفا نام کاربری و رمز عبور را وارد نمایید");
 		}
 
 	}
 
+	private void showHomePage() {
+		try {
+			Stage stage;
+			Parent root;
+			stage = (Stage) loginButton.getScene().getWindow();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("../../view/userAccount/UserAccount.fxml"));
+			root = (Parent) loader.load();
+			UserAccountController userAccountController = loader.<UserAccountController> getController();
+			userAccountController.setLoggedInUser(loggedInUser);
+			userAccountController.getProfileController().setLoggedInUser(loggedInUser);
+			userAccountController.getProfileController().setAllLabels();
+
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * Called when there is an action on SignUpButton
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
 	 */
 	@FXML
 	private void handleSignupButton() throws IOException {
-		Stage stage; 
+		Stage stage;
 		Parent root;
 		stage = (Stage) signUpButton.getScene().getWindow();
 		root = FXMLLoader.load(getClass().getResource("../../view/userAccount/SignUpUI.fxml"));
@@ -96,7 +99,6 @@ public class LoginController {
 		stage.setScene(scene);
 		stage.show();
 	}
-
 
 	/**
 	 * Is called by the main application to give a reference back to itself.
