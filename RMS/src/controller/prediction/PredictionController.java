43cbed9ecd2fac4af48a787cdfeb37b4ab4a8286
Mehrnoosh <mirtaheri.sp72@gmail.com>
2016-07-15 12:53:08 +0430

    database utf-8 support

--- RMS/src/controller/prediction/PredictionController.java
+++ RMS/src/controller/prediction/PredictionController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import logic.organization.project.Project;
-import model.organization.project.ProjectModel;
 
 public class PredictionController {
 
